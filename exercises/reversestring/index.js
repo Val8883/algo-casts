09d255cd024e314849520bac739977fa42d563f1
Valentyn Filippov <valentine@Valentyns-MacBook-Pro.local>
2020-08-17 03:05:42 +0300

    complite reversestring

--- exercises/reversestring/index.js
+++ exercises/reversestring/index.js
@@ -6,6 +6,31 @@
 //   reverse('hello') === 'olleh'
 //   reverse('Greetings!') === '!sgniteerG'
 
-function reverse(str) {}
+function reverse(str) {
+  return str
+    .split('')
+    .reverse()
+    .join('');
+}
 
-module.exports = reverse;
+function reverse2(str) {
+  let newStr = '';
+  for (let i = str.length; i > 0; i--) {
+    newStr += str[i - 1];
+  }
+  return newStr;
+}
+
+function reverse3(str) {
+  let reversed = '';
+  for (let char of str) {
+    reversed = char + reversed;
+  }
+  return reversed;
+}
+
+function reverse4(str) {
+  return str.split('').reduce((acc, char) => char + acc, '');
+}
+
+module.exports = reverse4;
